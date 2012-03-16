6cf08b095f34847e7dce6278318c6919f22cd061
Logan Laughlin <loganlaughlin@gmail.com>
2012-03-16 16:42:53 -0500

    Finally added some code.

---
+++ Unprotected.Net/Unprotected.Net Sample/Sample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Unprotected.Net_Sample
+{
+    using Net;
+
+    public class Sample
+    {
+        public static void Main(string[] args)
+        {
+            var foo = new Foo();
+            Console.WriteLine("MyPublicString before alteration: {0}", foo.MyPublicString);
+            Console.WriteLine("GetMyPrivateString before alteration: {0}", foo.GetMyPrivateString);
+            Console.WriteLine("GetMyPrivateInt before alteration: {0}", foo.GetMyPrivateInt);
+            Console.WriteLine();
+
+            dynamic exposer = new PrivatePartsExposer<Foo>(foo);
+
+            exposer.MyPublicString = "Altered public string";
+            exposer.MyPrivateString = "Altered private string";
+            exposer.MyPrivateInt = 5555;
+
+            Console.WriteLine("After public string alteration: {0}", foo.MyPublicString);
+            Console.WriteLine("After private string alteration: {0}", foo.GetMyPrivateString);
+            Console.WriteLine("After private int alteration: {0}", foo.GetMyPrivateInt);
+            Console.WriteLine();
+
+            Console.WriteLine("Value of private string getter: {0}", exposer.MyPrivateStringGetter);
+            Console.WriteLine("Value of private int getter: {0}", exposer.MyPrivateIntGetter);
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+
+    public class Foo
+    {
+        public string MyPublicString { get; set; }
+        private string MyPrivateString { get; set; }
+
+        private int MyPrivateInt { get; set; }
+
+        private string MyPrivateStringGetter
+        {
+            get { return "Got Private String"; }
+        }
+
+        private int MyPrivateIntGetter
+        {
+            get { return 9999; }
+        }
+
+        public string GetMyPrivateString { get { return MyPrivateString; } }
+        public int GetMyPrivateInt { get { return MyPrivateInt; } }
+
+        public Foo()
+        {
+            MyPublicString = "Default MyString";
+            MyPrivateString = "Default MyPrivateString";
+            MyPrivateInt = 1000;
+        }
+    }
+}
---
+++ Unprotected.Net/Unprotected.Net/PrivatePartsExposer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Unprotected.Net
+{
+    using System.Dynamic;
+    using System.Reflection;
+
+
+    public class PrivatePartsExposer<T> : DynamicObject
+    {
+        private Dictionary<string, PropertyInfo> Properties { get; set; }
+
+        private T Source { get; set; }
+
+        public PrivatePartsExposer(T source)
+        {
+            Source = source;
+            var type = typeof(T);
+            Properties = type.GetProperties()
+                .Union(type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance))
+                .ToDictionary(pi => pi.Name);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            PropertyInfo property;
+            result = null;
+            if (Properties.TryGetValue(binder.Name, out property))
+            {
+                result = property.GetValue(Source, null);
+                return true;
+            }
+            return false;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            PropertyInfo property;
+            if (Properties.TryGetValue(binder.Name, out property))
+            {
+                property.SetValue(Source, value, null);
+                return true;
+            }
+            return false;
+        }
+    }
+}
